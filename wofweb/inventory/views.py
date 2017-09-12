559db711cac6372c963f83c559d9ca65fbabd4ee
Brett R. Ward <brett.r.ward@rrd.com>
2017-09-12 12:56:37 -0600

    Got site working with home and inventory app. Commiting base working site.

--- wofweb/inventory/views.py
+++ wofweb/inventory/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    ctx = {}
+    ctx['title'] = 'WoF Inventory'
+    return render(request, 'inventory/index.html', ctx)
--- wofweb/wofweb/settings.py
+++ wofweb/wofweb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wofweb',
     'home',
     'inventory',
 ]
@@ -118,5 +119,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# import local settings
+try:
+    from powerweb.local_settings import *
+    addLocalApps(INSTALLED_APPS)
+except ImportError:
+    pass
